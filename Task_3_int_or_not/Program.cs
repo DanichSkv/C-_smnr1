bc01b3399b2a91bd45a995e9a813a8124e08657a
DaniilSkv <danichskv@gmail.com>
2023-09-28 00:32:02 +0500

    Task 3 is complete

--- Task_3_int_or_not/Program.cs
+++ Task_3_int_or_not/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+//4 -> да
+//-3 -> нет
+//7 -> нет
+
+using System.Net.Security;
+
+int num1;
+Console.Write("Привет, эта программа проверяет четность числа! Введите чило: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 % 2 == 0)
+{
+    Console.WriteLine("Число чётное!");
+}
+else
+{
+    Console.WriteLine("Число нечётное!");
+}
